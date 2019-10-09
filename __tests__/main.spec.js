b42f37b5149d904e59ebc7f2aa691dee9a2900d7
Lester Jan Artienda <artiele@oocl.com>
2019-10-10 00:09:00 +0800

    Refactor Code

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -27,3 +27,60 @@ it('should display receipt', () => {
         "Price: 20"
     expect(printReceipt(collectionBarcode, db)).toBe(result);
 });
+
+it('should display error since not all barcodes are valid', () => {
+    const collectionBarcode = ['1003', '0003', '0005', '0003'];
+    const db =
+        [
+            { "id": "0001", "name": "Coca Cola", "price": 3 },
+            { "id": "0002", "name": "Diet Coke", "price": 4 },
+            { "id": "0003", "name": "Pepsi-Cola", "price": 5 },
+            { "id": "0004", "name": "Mountain Dew", "price": 6 },
+            { "id": "0005", "name": "Dr Pepper", "price": 7 },
+            { "id": "0006", "name": "Sprite", "price": 8 },
+            { "id": "0007", "name": "Diet Pepsi", "price": 9 },
+            { "id": "0008", "name": "Diet Mountain Dew", "price": 10 },
+            { "id": "0009", "name": "Diet Dr Pepper", "price": 11 },
+            { "id": "0010", "name": "Fanta", "price": 12 }
+        ]
+    const result = '[ERROR]: Barcodes are not valid: 1003'
+    expect(printReceipt(collectionBarcode, db)).toBe(result);
+});
+
+it('should display error since barcode is not valid', () => {
+    const collectionBarcode = [''];
+    const db =
+        [
+            { "id": "0001", "name": "Coca Cola", "price": 3 },
+            { "id": "0002", "name": "Diet Coke", "price": 4 },
+            { "id": "0003", "name": "Pepsi-Cola", "price": 5 },
+            { "id": "0004", "name": "Mountain Dew", "price": 6 },
+            { "id": "0005", "name": "Dr Pepper", "price": 7 },
+            { "id": "0006", "name": "Sprite", "price": 8 },
+            { "id": "0007", "name": "Diet Pepsi", "price": 9 },
+            { "id": "0008", "name": "Diet Mountain Dew", "price": 10 },
+            { "id": "0009", "name": "Diet Dr Pepper", "price": 11 },
+            { "id": "0010", "name": "Fanta", "price": 12 }
+        ]
+    const result = '[ERROR]: Barcodes are not valid:'
+    expect(printReceipt(collectionBarcode, db)).toBe(result);
+});
+
+it('should display error since barcodes are required', () => {
+    const collectionBarcode = [];
+    const db =
+        [
+            { "id": "0001", "name": "Coca Cola", "price": 3 },
+            { "id": "0002", "name": "Diet Coke", "price": 4 },
+            { "id": "0003", "name": "Pepsi-Cola", "price": 5 },
+            { "id": "0004", "name": "Mountain Dew", "price": 6 },
+            { "id": "0005", "name": "Dr Pepper", "price": 7 },
+            { "id": "0006", "name": "Sprite", "price": 8 },
+            { "id": "0007", "name": "Diet Pepsi", "price": 9 },
+            { "id": "0008", "name": "Diet Mountain Dew", "price": 10 },
+            { "id": "0009", "name": "Diet Dr Pepper", "price": 11 },
+            { "id": "0010", "name": "Fanta", "price": 12 }
+        ]
+    const result = '[ERROR]: Barcodes are required'
+    expect(printReceipt(collectionBarcode, db)).toBe(result);
+});
